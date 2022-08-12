f2c438377ac4b96d7e3a86cdf02b56a49b23883b
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-12 11:45:33 +0200

    api with sqlite works

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
 import express from 'express';
+import sqlite3 from 'sqlite3';
 
 const app = express();
 const PORT = 3004;
 
+const getRecordsWithSql = (sql) => {
+	return new Promise((resolve, reject) => {
+		const db = new sqlite3.Database('./northwind_database.sqlite');
+		db.all(sql, function (err, records) {
+			if (records === undefined) {
+				reject(err);
+			} else if (records.length === 0) {
+				resolve([]);
+			} else {
+				resolve(records);
+			}
+		});
+		db.close();
+	});
+};
 
 app.get('/', (req, res) => {
 	res.send('API with SQLite');
 });
 
-app.get('/employees', (req, res) => {
-	const employees = [
-		{
-			firstName: "fff",
-			lastName: "lll"
-		},
-		{
-			firstName: "fff",
-			lastName: "lll"
-		}
-	];
+app.get('/employees', async (req, res) => {
+	const employees = await getRecordsWithSql('SELECT * FROM Employees');
+	console.log(employees);
+		
+	res.send(employees);
+});
+
+app.get('/uk-employees', async (req, res) => {
+	const employees = await getRecordsWithSql('SELECT * FROM Employees WHERE Country = "UK"');
+	console.log(employees);
+		
+	res.send(employees);
+});
+
+app.get('/usa-employees', async (req, res) => {
+	const employees = await getRecordsWithSql('SELECT * FROM Employees WHERE Country = "USA"');
+	console.log(employees);
+		
 	res.send(employees);
 });
 
